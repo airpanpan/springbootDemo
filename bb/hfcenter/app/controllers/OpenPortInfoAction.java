93ebb00d84bc3cb0d06fbf8f887a65872a6c2cbd
airpanpan <404184592@qq.com>
2019-08-19 17:06:22 +0800

    tuisong

---
+++ bb/hfcenter/app/controllers/OpenPortInfoAction.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import play.mvc.Controller;
+
+
+
+    /**
+    * @ClassName: ValidationAction
+    * @Description: 验证客户类
+    * @author zhiqiang zeng
+    * @date 2017年3月2日
+    * @version V1.0
+    */
+public class OpenPortInfoAction extends Controller{
+
+	private static Log log = LogFactory.getLog(OpenPortInfoAction.class);
+	
+	
+	
+	
+	public static void interiorPortInfo(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
